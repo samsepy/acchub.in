cbe9bc285f9570662b453be546b531f3edcc3811
Hajime Todo <samsepylot@gmail.com>
2022-08-01 23:30:22 +0900

    fix lint & prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 
-import styles from "@/styles/Home.module.css";
-
 import type { NextPage } from "next";
+
+import styles from "@/styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (

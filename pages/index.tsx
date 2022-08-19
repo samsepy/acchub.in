7500592f01baaf2ca419888b89886ff384489bfd
Hajime Todo <samsepylot@gmail.com>
2022-08-19 16:38:36 +0900

    トップのデザイン整える

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,75 +58,84 @@ const Home: NextPage = ({
   };
 
   return (
-    <div>
-      {users.map((user) => {
-        return (
-          <div className="p-4 mb-2 border flex" key={user.id}>
-            <div className="mr-4">
-              <Image
-                src={user.image ?? "https://i.imgur.com/CgUjxSp.png"}
-                className="rounded-full"
-                height={75}
-                width={75}
-                alt="user logo"
-              />
-            </div>
-            <div>
-              <div>{user?.profile?.screenName}</div>
-              <div className="flex">
-                {user.services.map((service) => {
-                  return (
-                    <div className="mr-2">
-                      <a
-                        href={new URL(service.screenName, service.service.url)}
-                      >
-                        <DynamicFaIcon name={service.service.icon} />
-                      </a>
-                    </div>
-                  );
-                })}
+    <div className="bg-gray-50 h-screen">
+      <div className="container max-w-3xl pt-5">
+        <div className="mb-10">
+        {users.map((user) => {
+          return (
+            <div
+              className="p-4 mb-2 bg-white border rounded-lg flex"
+              key={user.id}
+            >
+              <div className="mr-4">
+                <Image
+                  src={user.image ?? "https://i.imgur.com/CgUjxSp.png"}
+                  className="rounded-full"
+                  height={75}
+                  width={75}
+                  alt="user logo"
+                />
+              </div>
+              <div>
+                <div>{user?.profile?.screenName}</div>
+                <div className="flex">
+                  {user.services.map((service) => {
+                    return (
+                      <div className="mr-2">
+                        <a
+                          href={
+                            new URL(service.screenName, service.service.url)
+                          }
+                        >
+                          <DynamicFaIcon name={service.service.icon} />
+                        </a>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-      <div className="flex justify-center">
-        <div className="mb-3 xl:w-96">
-          <div className="input-group relative flex flex-wrap items-stretch w-full mb-4">
-            <input
-              type="search"
-              className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="button-addon2"
-              name="accountId"
-              onChange={(e) => {
-                setKeyword(e.target.value);
-              }}
-            />
-            <button
-              className="btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
-              type="button"
-              id="button-addon2"
-              onClick={clickButton}
-              disabled={!keyword}
-            >
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="search"
-                className="w-4"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
+          );
+        })}
+        </div>
+        <div className="flex justify-center">
+          <div className="mb-3 xl:w-96">
+            <div className="input-group relative flex items-stretch w-full mb-4">
+              <input
+                type="search"
+                className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="button-addon2"
+                name="accountId"
+                onChange={(e) => {
+                  setKeyword(e.target.value);
+                }}
+              />
+              <button
+                className="btn inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-800 hover:shadow-lg focus:bg-gray-800  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out flex items-center"
+                type="button"
+                id="button-addon2"
+                onClick={clickButton}
+                disabled={!keyword}
               >
-                <path
-                  fill="currentColor"
-                  d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
-                ></path>
-              </svg>
-            </button>
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="search"
+                  className="w-4"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+                  ></path>
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       </div>

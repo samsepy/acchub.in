00008172240e42f430281980745fa12f4643706f
Hajime Todo <samsepylot@gmail.com>
2022-08-19 15:38:56 +0900

    ドロップダウン表示非表示

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 import type { FC } from "react";
 
 export const Header: FC = () => {
   const { data: session } = useSession();
+  const [dropDownToggle, setDropDownToggle] = useState(false);
+
   if (session) {
     return (
       <div className="flex items-center justify-between py-3 px-2">
@@ -18,33 +21,36 @@ export const Header: FC = () => {
               height={40}
               width={40}
               alt="user logo"
+              onClick={() => setDropDownToggle(!dropDownToggle)}
             />
           </button>
-          <div
-            className="absolute top-12 right-0 border rounded px-4 py-3"
-            style={{ width: "200px" }}
-          >
-            <div className="mb-3">
-              <Link href="/me/profile">
-                <a>
-                  <div>
-                    {session.user?.profile?.displayName ?? "名前未設定"}
-                  </div>
-                  <div className="text-gray-500">
-                    {session.user?.profile?.screenName ?? "ID未設定"}
-                  </div>
-                </a>
-              </Link>
+          {dropDownToggle && (
+            <div
+              className="absolute top-12 right-0 border rounded px-4 py-3"
+              style={{ width: "200px" }}
+            >
+              <div className="mb-3">
+                <Link href="/me/profile">
+                  <a>
+                    <div>
+                      {session.user?.profile?.displayName ?? "名前未設定"}
+                    </div>
+                    <div className="text-gray-500">
+                      {session.user?.profile?.screenName ?? "ID未設定"}
+                    </div>
+                  </a>
+                </Link>
+              </div>
+              <hr className="mb-3" />
+              <div className="mb-3">
+                <Link href="/me/settings">
+                  <a>ユーザー設定</a>
+                </Link>
+              </div>
+              <hr className="mb-3" />
+              <button onClick={() => signOut()}>Sign out</button>
             </div>
-            <hr className="mb-3" />
-            <div className="mb-3">
-              <Link href="/me/settings">
-                <a>ユーザー設定</a>
-              </Link>
-            </div>
-            <hr className="mb-3" />
-            <button onClick={() => signOut()}>Sign out</button>
-          </div>
+          )}
         </div>
       </div>
     );

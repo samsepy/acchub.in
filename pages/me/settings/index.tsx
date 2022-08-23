d5c1de225cdfcf1bea67c8ada870dd7c540bc11c
Hajime Todo <samsepylot@gmail.com>
2022-08-23 12:13:43 +0900

    ページ仮置き

---
+++ pages/me/settings/index.tsx
@@ -0,0 +1,11 @@
+const MeSettingsIndex: NextPage = () => {
+  return (
+    <div className="bg-gray-50 h-screen">
+      <div className="container max-w-3xl pt-5">
+        <h1 className="text-lg mb-2">User settings</h1>
+      </div>
+    </div>
+  );
+};
+
+export default MeSettingsIndex;

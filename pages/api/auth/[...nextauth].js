77264f2cd76c973468d733ab24aa26e0c37a0a56
Hajime Todo <samsepylot@gmail.com>
2022-10-06 15:19:48 +0900

    next auth secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,6 +12,7 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session, user }) {
       if (session?.user) {

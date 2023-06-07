684d8c717f4749c0ee6b52590462c139d8449ce3
Hajime Todo <samsepylot@gmail.com>
2023-06-07 17:13:42 +0900

    Revert "アイコン型追加"

--- components/DynamicFaIcon/DynamicFaIcon.tsx
+++ components/DynamicFaIcon/DynamicFaIcon.tsx
@@ -1,12 +1,7 @@
 import type { FC } from "react";
 import * as Icons from "react-icons/fa";
 
-type Props = {
-  name: keyof typeof Icons;
-};
-
-export const DynamicFaIcon: FC<Props> = ({ name }) => {
-  // eslint-disable-next-line
+export const DynamicFaIcon: FC = ({ name }) => {
   const IconComponent = Icons[name];
 
   if (!IconComponent) {
